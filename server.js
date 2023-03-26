f6b9cd2252a550a22eae42531f173a3bea6649cc
Sarrthi Jasrotia <sarrthijasrotia05@gmail.com>
2023-03-26 10:42:34 -0500

    test

--- server.js
+++ server.js
@@ -1,46 +1,46 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 
-const express = require("express");
+// const express = require("express");
 
-const cors = require("cors");
-const bodyParser = require("body-parser");
-require("dotenv").config();
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// require("dotenv").config();
 
-const { Configuration, OpenAIApi } = require("openai");
-
-
-
-const configuration = new Configuration({
-
-  apiKey: process.env.OPEN_API_KEY,
+// const { Configuration, OpenAIApi } = require("openai");
 
 
-});
-const openai = new OpenAIApi(configuration);
 
-// Set up the server
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+// const configuration = new Configuration({
 
-// Set up the ChatGPT endpoint
-app.post("/chat", async (req, res) => {
-	// Get the prompt from the request
-	const { prompt } = req.body;
+//   apiKey: process.env.OPEN_API_KEY,
 
-	// Generate a response with ChatGPT
-	const completion = await openai.createCompletion({
-		model: 'text-davinci-002',
-		prompt: prompt,
-		max_tokens: 2048,
-	});
 
-	res.send(completion.data.choices[0].text);
-});
+// });
+// const openai = new OpenAIApi(configuration);
 
-// Start the server
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+// // Set up the server
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+
+// // Set up the ChatGPT endpoint
+// app.post("/chat", async (req, res) => {
+// 	// Get the prompt from the request
+// 	const { prompt } = req.body;
+
+// 	// Generate a response with ChatGPT
+// 	const completion = await openai.createCompletion({
+// 		model: 'text-davinci-002',
+// 		prompt: prompt,
+// 		max_tokens: 2048,
+// 	});
+
+// 	res.send(completion.data.choices[0].text);
+// });
+
+// // Start the server
+// const port = 8080;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
